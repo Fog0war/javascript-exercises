821c2e9e339467ac664ff50e395fc6b81bc979ae
Fog0war <130712042+Fog0war@users.noreply.github.com>
2023-07-10 14:03:58 -0400

    exercises 1-4 completed and tested

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringRep = "";
+    if (num < 0 || num === NaN) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {
+            stringRep += `${string}`;
+        };
+        return stringRep;
+    };    
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
+    const priArray = Array.from(arguments);
+    const secArray = priArray[0];
+    
+        for (let i = 1; i < priArray.length; i++) {
+            for (let j = 0; j < secArray.length; j++) {
+                if (secArray[j] === priArray[i]) {
+                    secArray.splice(j, 1);
+                    j--;
+                };
+            };
+        };
+        return secArray;
+    };
 
-};
+// we want to compare priArray[1, 2, ... n] against secArray[0, 1, ... n], and if priArray[n] === any secArray[n2]; secArray.pop[n2], then
+// return secArray.
 
 // Do not edit below this line
 module.exports = removeFromArray;
